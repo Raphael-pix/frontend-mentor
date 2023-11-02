7195841e9cf644c36c3a831dc8acee15ca14b866
Raphael Kaira <kaira.victor22@students.dkut.ac.ke>
2023-11-02 09:37:12 +0300

    portfolio

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const arrow = document.querySelector(".arrow");
+const arrow = document.querySelector(".arrow1");
 console.log(arrow);
+const dropdown = document.querySelector(".dropdown-menu-features")
+console.log(dropdown);
+
+
+
+ let isVisible = false;
+ arrow.addEventListener("click", ()=>{
+  if (isVisible){
+    dropdown.style.display = "none";
+  }else{
+    dropdown.style.display = "block";
+  }
+  isVisible = !isVisible;
+  console.log(isVisible)
+ })
