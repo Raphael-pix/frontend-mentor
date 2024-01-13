6fb8cf0fb030c94fc082ce8219122e979370f00c
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2024-01-13 10:20:30 +0300

    fixed styling for success page and added some JS

--- newsletter/script.js
+++ newsletter/script.js
@@ -1,11 +1,13 @@
 const form = document.getElementById("form")
 function validate() {
-    let emailInput = document.getElementById("email")
-    let errorElement = document.querySelector(".error");
+    const emailInput = document.getElementById("email")
+    const errorElement = document.querySelector(".error");
   
-    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    let emailValue = emailInput.value
   
-    if (!emailInput.value.match(emailPattern)) {
+    if (!emailValue.match(emailPattern)) {
       errorElement.textContent = "valid email required";
       emailInput.setAttribute("style","background-color:#ff625756;;border:1px solid #ff6257;")
       return false; 
@@ -13,7 +15,7 @@ function validate() {
   
     errorElement.textContent = ""; 
     emailInput.setAttribute("style","background-color:white;;border: 1px solid rgb(128, 128, 128);")
-    window.location.href = "success.html";
+    window.location.href = "success.html?emailValue="+encodeURIComponent(emailValue);
     return true;
   }
   document.addEventListener("DOMContentLoaded", function() {
@@ -23,6 +25,13 @@ function validate() {
     })
   });
 
+  document.addEventListener("DOMContentLoaded",function(){
+      let urlParams = new URLSearchParams(window.location.search);
+      let email = urlParams.get("emailValue");
+
+      let successEmail = document.getElementById("successEmail");
+      successEmail.innerHTML =  decodeURIComponent(email);
+  })
   function dismiss(){
     window.location.href = "index.html";
   }
