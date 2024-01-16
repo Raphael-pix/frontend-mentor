0fad05b68e597cd5b5df1473894f960be507b51c
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2024-01-16 17:33:02 +0300

    added underline under the nav links

--- space tourism/script.js
+++ space tourism/script.js
@@ -1,5 +1,20 @@
 const nav = document.querySelector(".nav-bar")
+const links = document.querySelectorAll(".body-nav a")
 
 function toggleMenu(){
     nav.classList.toggle("visible")
 }
+
+
+links.forEach(link=>{
+    console.log(link)
+})
+console.log(window.location.href)
+function underline(){
+    links.forEach(link =>{
+        if(link.href === window.location.href){
+            link.classList.toggle("active")
+        }
+    })
+}
+window.addEventListener("load",underline())
