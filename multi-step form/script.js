36bd828f31e082f5ab8aabb0f392758170abb50f
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2024-01-23 22:36:13 +0300

    added form validation

--- multi-step form/script.js
+++ multi-step form/script.js
@@ -9,3 +9,28 @@ function active(){
     })
 }
 window.addEventListener("load",active())
+
+
+const inputs = Array.from(document.getElementsByClassName("input"))
+const error = document.querySelectorAll(".error")
+const form =document.querySelector("#form")
+console.log(form)
+function checkRequired(){
+    inputs.forEach(input=>{
+        if(input.value === ""){
+            error[i].textContent = "This field is required"
+            input.classList.add("error")
+        }else{
+            error[i].textContent = ""
+            input.classList.remove("error")
+        }
+        i++
+    },i=0)
+}
+
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    if(checkRequired()){
+        window.location.href = "plan.html"
+    }
+})
